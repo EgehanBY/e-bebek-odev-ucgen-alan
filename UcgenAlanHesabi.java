83287bdce885ec9c25f375602b13120497ac8019
Egehan Buğra YÜKSEL <ege.han@windowslive.com>
2022-11-09 22:01:33 +0300

    Create UcgenAlanHesabi.java

---
+++ UcgenAlanHesabi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class UcgenAlanHesabi {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        double a,b,c,cevre,alan, u;
+        System.out.println("Alanını hesaplamak istediğiniz üçgenin kenar uzunluklarını giriniz. Lütfen girdiğiniz değerlerin bir üçgen oluşturup oluşturmadığına dikkat ediniz.");
+        System.out.print("1. kenar uzunluğunu giriniz : ");
+        a=sc.nextDouble();
+        System.out.print("2. kenar uzunluğunu giriniz : ");
+        b=sc.nextDouble();
+        System.out.print("3. kenar uzunluğunu giriniz : ");
+        c=sc.nextDouble();
+        boolean ucgenSarti;
+        // 1. ve 2. kenar toplamının 3. kenardan küçük olması şartı koşulur
+        ucgenSarti=  (((a+b)>c) && ((a+c)>b) && ((b+c)>a));
+        cevre=a+b+c;
+        u=cevre/2;
+        alan= Math.sqrt(u*(u-a)*(u-b)*(u-c));
+
+        System.out.println("Üçgeninizin alanı : " +alan);
+
+
+    }
+
+}
